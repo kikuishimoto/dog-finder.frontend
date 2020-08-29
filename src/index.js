42625d0f4dce2836f394fb3a1c3ad532cd04d8fb
kikuishimoto <kikuish@gmail.com>
2020-08-28 19:14:05 -0700

    created event listeners for create

--- src/index.js
+++ src/index.js
@@ -1,17 +1,66 @@
 const dogList = document.getElementById("dogs-list")
+const dogForm = document.getElementById("dog-form")
+const dogImage = document.getElementById("dog-image")
+const dogBreed = document.getElementById("dog-breed")
+const dogName = document.getElementById("dog-name")
+const dogLocation = document.getElementById("dog-location")
+const dogAge = document.getElementById("dog-age")
+const dogEmail = document.getElementById("dog-email")
+const dogDescription = document.getElementById("dog-description")
+
 function fetchDogs() {
-    fetch('http://127.0.0.1:3000/breeds')
+    fetch('http://127.0.0.1:3000/dogs')
     .then(res => res.json())
-    .then(addDogsToDom)
+    .then(addDogs)
 }
-function addDogsToDom(response){
-    
-    data = response.data
-    
-    data.forEach(dog => {
-        dogList.innerHTML += `<li> id="dog-${dog.id}" ${dog.attributes.name}</li>`
+function addDogs(response){
+    response.data.forEach(dog => {
+       addDogToDom(dog)
     })
+}
+function addDogToDom(dog){
+    dogList.innerHTML += `
+    <div id="dog-${dog.id}">
+        <li>
+        <span class="image-url"> <img src="${dog.image_url}" width="100" height="100"> </span><br>
+        <span class="name">Name: ${dog.name}</span><br>
+        <span class="location">Location: ${dog.location}</span><br>
+        <span class="age">Age: ${dog.age}</span><br>
+        <span class="breed"> Breed: ${dog.breed}</span><br>
+        <span class="description">Description: ${dog.description}</span><br>
+        <span class="email"> Contact info: ${dog.email} </span>
+        </li>
+    </div>`
+}
+function handleFormSubmit(e){
+    e.preventDefault()
+    let newDogObj = {
+        image_url: dogImage.value,
+        name: dogName.value,
+        location: dogLocation.value,
+        age: dogAge.value,
+        breed: dogBreed.value,
+        description: dogDescription.value,
+        email: dogEmail.value
+    }
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(newDogObj)
+    }
+
+    fetch('http://127.0.0.1:3000/dogs', configObj)
+    .then(res => res.json())
+    .then(response => {
+        addDogToDom(response)
+    })
+
+    dogForm.reset()
 }
 document.addEventListener('DOMContentLoaded', () => {
     fetchDogs()
+    dogForm.addEventListener('submit', handleFormSubmit)
 })
