b7e30943feee4474a323b2eb0bdbcb62c2d4c4a3
kikuishimoto <kikuish@gmail.com>
2020-08-30 21:14:05 -0700

    commented out update funtion

--- src/dog.js
+++ src/dog.js
@@ -21,18 +21,18 @@ class Dog{
         if (e.target.className === "delete"){
             let id = e.target.dataset.id
              deleteDog(id)
-        } else if(e.target.className === 'update'){
-             let dogId = e.target.dataset.id
-             e.target.className = "save"
-             e.target.innerText = "Save"
-             addUpdateDogFields(dogId)
-         } else if(e.target.className === 'save'){
-             let dogId = e.target.dataset.id
-             e.target.className = "update"
-             e.target.innerText = "Update"
-             debugger
-             dogsAdapter.sendPatchRequest(dogId)
-         }
+        }
+       // else if(e.target.className === 'update'){
+        //      let dogId = e.target.dataset.id
+        //      e.target.className = "save"
+        //      e.target.innerText = "Save"
+        //      addUpdateDogFields(dogId)
+        //  } else if(e.target.className === 'save'){
+        //      let dogId = e.target.dataset.id
+        //      e.target.className = "update"
+        //      e.target.innerText = "Update"
+        //      dogsAdapter.sendPatchRequest(dogId)
+        //  }
      }
     attachToDom(){
         this.dogList.append(this.fullRender())
@@ -50,21 +50,21 @@ class Dog{
         <span class="email"> ${this.email} </span>
         </li>
         <button class="delete" data-id="${this.id}">Delete</button>
-        <button class="update" data-id="${this.id}">Update</button>
+        
     
         `
-        return this.element
+        return this.element;
     }
-    updateDogOnDom({image_url, name, location, age, breed, description, email}){
-        //let liDog = document.querySelector(`#item-${item.id} li`)
-        this.image_url = image_url
-        this.name = name 
-        this.location =location 
-        this.age = age
-        this.breed = breed
-        this.description = description
-        this.email = email 
-        this.fullRender()
-    }
+    // updateDogOnDom({image_url, name, location, age, breed, description, email}){
+    //     //let liDog = document.querySelector(`#item-${item.id} li`)
+    //     this.image_url = image_url
+    //     this.name = name 
+    //     this.location =location 
+    //     this.age = age
+    //     this.breed = breed
+    //     this.description = description
+    //     this.email = email 
+    //     this.fullRender()
+    // }
 
 }
--- src/index.js
+++ src/index.js
@@ -10,8 +10,24 @@ const dogDescription = document.getElementById("dog-description")
 const allDogs = document.getElementById("all-dogs")
 const dogsAdapter = new DogsAdapter
 
+function addDogToDom(dog){
+    dogList.innerHTML += `
+    <div id="dog-${dog.id}">
+    <li>
+    <span class="image-url"> <img src="${dog.attributes.image_url}" width="100" height="100"> </span><br>
+    <label> Name: </label>
+    <span class="name">${dog.attributes.name}</span><br>
+    <span class="location">${dog.attributes.location}</span><br>
+    <span class="age">${dog.attributes.age}</span><br>
+    <span class="breed"> ${dog.attributes.breed.name}</span><br>
+    <span class="description">${dog.attributes.description}</span><br>
+    <span class="email"> ${dog.attributes.email} </span>
+    </li>
+    <button class="delete" data-id="${dog.id}">Delete</button>
+    
 
-
+    </div>`
+}
 
 
 function handleFormSubmit(e){
@@ -61,37 +77,39 @@ function deleteDog(id){
     dog.remove()
 
 }
-function addUpdateDogFields(dogId){
-    let dog = document.querySelector(`#dog-${dogId} li`)
-    let image_url = dog.querySelector('.image-url').innerHTML.split(" ")[2].split('"')[1]
-    let name = dog.querySelector('.name').innerText 
-    let location = dog.querySelector('.location').innerText
-    let age = dog.querySelector('.age').innerText
-    let breed = dog.querySelector('.breed').innerText
-    let description = dog.querySelector('.description').innerText
-    let email = dog.querySelector('.email').innerText
+// function addUpdateDogFields(dogId){
+//     let dog = document.querySelector(`#dog-${dogId} li`)
+//     let image_url = dog.querySelector('.image-url').innerHTML.split(" ")[2].split('"')[1]
+//     let name = dog.querySelector('.name').innerText 
+//     let location = dog.querySelector('.location').innerText
+//     let age = dog.querySelector('.age').innerText
+//     let breed = dog.querySelector('.breed').innerText
+//     let description = dog.querySelector('.description').innerText
+//     let email = dog.querySelector('.email').innerText
 
 
-    let updateForm = `
-    <input type="text" value="${image_url}" name="image_url" id="update-image_url-${dogId}">
-    <input type="text" value="${name}" name="name" id="update-name-${dogId}">
-    <input type="text" value="${location}" name="location" id="update-location-${dogId}">
-    <input type="text" value="${age}" name="age" id="update-age-${dogId}">
-    <input type="text" value="${breed}" name="breed" id="update-breed-${dogId}">
-    <input type="text" value="${description}" name="description" id="update-description-${dogId}">
-    <input type="text" value="${email}" name="email" id="update-email-${dogId}">
-    `
+//     let updateForm = `
+//     <input type="text" value="${image_url}" name="image_url" id="update-image_url-${dogId}">
+//     <input type="text" value="${name}" name="name" id="update-name-${dogId}">
+//     <input type="text" value="${location}" name="location" id="update-location-${dogId}">
+//     <input type="text" value="${age}" name="age" id="update-age-${dogId}">
+//     <input type="text" value="${breed}" name="breed" id="update-breed-${dogId}">
+//     <input type="text" value="${description}" name="description" id="update-description-${dogId}">
+//     <input type="text" value="${email}" name="email" id="update-email-${dogId}">
+//     `
 
-    let formDiv = document.createElement('div')
-    formDiv.id = `update-form-${dogId}`
-    formDiv.innerHTML = updateForm
-    dog.append(formDiv)
-}
+//     let formDiv = document.createElement('div')
+//     formDiv.id = `update-form-${dogId}`
+//     formDiv.innerHTML = updateForm
+//     dog.append(formDiv)
+// }
+
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
     dogsAdapter.fetchDogs()
     dogForm.addEventListener('submit', handleFormSubmit)
+
 
 })
